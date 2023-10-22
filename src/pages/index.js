96a2143207ed0e7a0b004af6a5bfb0d81d057c9e
Jonathan Shyaka <jonashyaka2@gmail.com>
2023-10-22 19:58:26 +0200

    Adds header + Font

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col px-10 py-5">
-      <div className="flex items-center">
-        <span className="text-3xl font-bold text-pink">Airbnb</span>
+    <main className="flex min-h-screen flex-col">
+      <div className="flex items-center border-b-[1px] border-gray-light px-10 py-5">
+        <span className="text-3xl font-bold text-pink">Hotels&Co.</span>
       </div>
     </main>
   )
